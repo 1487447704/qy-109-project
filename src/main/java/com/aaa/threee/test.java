b06ae9d7000a240fc4cc7919572b111428446210
yk-sys <66992643+yk-sys@users.noreply.github.com>
2020-07-08 14:59:28 +0800

    Commit Message

---
+++ src/main/java/com/aaa/threee/test.java
@@ -0,0 +1,15 @@
+package com.aaa.threee;
+
+/**
+ * @Param
+ * @ClassName test
+ * @Description TODO
+ * @Author yk
+ * @Date 2020/7/8 0008 14:58
+ * @Return
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("this is demo");
+    }
+}
